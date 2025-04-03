6946d51da07f2a8239e8940266a9e2f29e9adb7d
spider747344 <9630003948sri@gmail.com>
2025-04-03 20:22:52 +0530

    activity1.py

---
+++ activity1.py
@@ -0,0 +1,10 @@
+#Input the value of terms
+n = int(input("Enter the value of terms: "))
+
+sum = 0 #initia;ise
+i = 1 #initialise
+while i<=n: #loop will from 1 to n
+    sum = sum+i
+    i = i+1
+
+    print("\nSum =", sum)
